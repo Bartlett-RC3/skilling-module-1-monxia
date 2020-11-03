432dec24947cbd3c8bbce02a50fa68095e7dd4ec
monxia <73493062+monxia@users.noreply.github.com>
2020-11-03 22:17:17 +0000

    Retrieve S1hw

--- Assets/Scripts/Homework/Session1Homework.cs
+++ Assets/Scripts/Homework/Session1Homework.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Session1Homework : MonoBehaviour {
-
+	Dictionary<string, string> ComponentTypes = new Dictionary<string, string>();
 	// Use this for initialization
-	void Start () {
+	public string intro = " Hi, My name is Meng Xia...";
+	public int JewelleryQty = 15;
+	public float WeightPerPcs = 194.55f;
+
+	public int[] InitialDimensions = new int[3];
+	public float[] MorePreceisely = {115.25f, 113.35f, 116.5f};
+
+	public List<string> MyReadingList = new List<string>();
+
+	private float Calculator(int _Qty, float _Weight)
+	{
+		float Result = _Qty * _Weight;
+		return Result;
+	}
 		
+		
+
+	void Start () {
+
+		float Result = Calculator(JewelleryQty, WeightPerPcs);
+		Debug.Log(" The total weight(in g) of the package is " + Result.ToString());
+		ComponentTypes["TypeA"] = "Sqaure";
+		ComponentTypes["TypeB"] = "Triangle";
+		ComponentTypes["TypeC"] = "Circle";
+
+		Debug.Log(message: $"{ComponentTypes["TypeA"]}" + " Belongs to TypeA");
+		Debug.Log(message: $"{ComponentTypes["TypeB"]}" + " Belongs to TypeB");
+		Debug.Log(message: $"{ComponentTypes["TypeC"]}" + " Belongs to TypeC");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
+
--- Assets/Scripts/Homework/Session2Homework.cs
+++ Assets/Scripts/Homework/Session2Homework.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Session2Homework : MonoBehaviour {
-
 	// Use this for initialization
+
+	public float[] collection = { 1.25f, 4, 2.56f, 3.05f, 5 };
+
 	void Start () {
-		
+		Debug.Log("The float/int reader:" + collection.ToString());		
 	}
 	
 	// Update is called once per frame
