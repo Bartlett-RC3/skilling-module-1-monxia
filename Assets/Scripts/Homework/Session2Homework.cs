7935da3f72838ab48c4e5658297b6111739e90d9
monxia <73493062+monxia@users.noreply.github.com>
2020-11-04 00:27:26 +0000

    Homework 2 WIP

--- Assets/Scripts/Homework/Session2Homework.cs
+++ Assets/Scripts/Homework/Session2Homework.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Session2Homework : MonoBehaviour {
 	// Use this for initialization
 
-	public float[] collection = { 1.25f, 4, 2.56f, 3.05f, 5 };
+	public string poet = "Less is more";
+	public float[] somefigures = { 1.24f, 3, 1.255f, 6 };
 
-	void Start () {
-		Debug.Log("The float/int reader:" + collection.ToString());		
+	void Start()
+
+	{
+
+		for (int i = 0; i <= poet.Length; i++)
+		{
+			char letter = poet[i];
+			Debug.LogFormat("'{0}';{1}", letter, char.GetUnicodeCategory(letter));
+		
+		}
+			
+
+		foreach( float figure in somefigures)
+        {
+			Debug.Log("CurrentFigure: " + figure.ToString());
+			if (figure.ToString().Contains("."))
+			{
+				Debug.Log(figure.ToString() + ": This figure is a float variable.");
+			}
+			else
+			{
+				Debug.Log(figure.ToString() + ": This figure is a int variable.");
+			}
+        }
+
+
 	}
-	
+
+
+
+
+
+		
 	// Update is called once per frame
 	void Update () {
 		
